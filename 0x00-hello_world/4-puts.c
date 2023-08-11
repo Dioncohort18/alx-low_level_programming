5a6ebcfd39b958b5941b1a2c8159945dfd5939d9
Dioncohort18 <martindion2020@gmail.com>
2023-08-11 09:59:19 -0700

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	// initializing strings
+	char strng[] = "Programming is like building a multilingual puzzle";
+	
+	// writing to stdout
+	puts(strng);
+	       
+	return (0);
+}
