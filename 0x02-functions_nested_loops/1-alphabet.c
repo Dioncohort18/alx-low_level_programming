3e1a6a8f5d675984c2c99573ae0470ac2053c64e
Dioncohort18 <martindion2020@gmail.com>
2023-08-15 12:52:23 -0700

    Alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing alphabet
+ * @void:void
+ *
+ * Description: prints alphabet using _putchar
+ * holberton.h: created header
+ * Return: none
+**/
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
