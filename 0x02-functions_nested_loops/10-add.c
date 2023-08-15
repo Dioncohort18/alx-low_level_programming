1a22db405f156055834971ecfe8b8bf18889dc0e
Dioncohort18 <martindion2020@gmail.com>
2023-08-15 13:36:12 -0700

    adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: first number
+ * @b: secon number
+ *
+ * main.h: created header
+ *
+ * Return: result
+**/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	if ((sum < 0 && a > 0 && b > 0) || (sum > 0 && a < 0 && b < 0))
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sum);
+	}
+}
