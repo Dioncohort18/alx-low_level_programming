b9ee583631b74f4c4470bf25f9afde1b05ea9500
Dioncohort18 <martindion2020@gmail.com>
2023-11-28 09:25:12 -0800

    1st fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
-#define ABS(x) ((x < 0) ? (x * (-1)) : (x))
+#ifndef ABS_VAL
+#define ABS_VAL
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 #endif
