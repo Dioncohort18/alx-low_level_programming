cff9f1a02ea023562bb374a60561e426c878a648
Dioncohort18 <martindion2020@gmail.com>
2023-12-07 12:20:48 -0800

    2nd trial

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,42 +1,29 @@
 #include "lists.h"
-
 /**
- * len - Calculates the length of a string.
- *
- * @str: Constant string
- *
- * Return: The length of the string
- */
-
-int len(const char *str)
-{
-	int count;
-
-	if (str == NULL)
-		return (0);
-	for (count = 0; str[count] != '\0'; count++)
-		;
-	return (count);
-}
-
-/**
- * add_node - function that adds a new node at the beginning of a list_t list.
- *
- * @head: Pointer to head of the linked list
- * @str: String to assigned to the node
- *
+ * add_node - function that adds a new node at the beginning of list_t list
+ * @head: pointer to pointer to list_t list
+ * @str: string to copy to new node
  * Return: the address of the new element, or NULL if it failed
- */
-
-list_t *add_node(list_t **head, char *str)
+ **/
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t *) malloc(sizeof(list_t));
+	list_t *new;
+	int i;
 
-	if (new_node == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
-	new_node->str = strdup(str);
-	new_node->len =  len(str);
-	new_node->next = (*head);
-	(*head) = new_node;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = i;
+	new->next = *head;
+	*head = new;
 	return (*head);
 }
