b53094670523a9e33defac23bf6803dd8daf3a37
Dioncohort18 <martindion2020@gmail.com>
2023-12-14 21:54:15 -0800

    1st trial

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,38 @@
 #include "main.h"
-/**
- * _strlen - returns the lenght of a string
- * @s: pointer to s
- *
- * Return: 0 on success
- *
- */
-int _strlen(const char *s)
-{
-	int count = 0;
 
-	if (s != '\0')
-	{
-		while (*(s + count) != '\0')
-			count++;
-	}
-	return (count);
-}
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: pointing to a string of 0 and 1 chars
- * Return: the converted number, or 0 if there is one or more chars in the
- *  string b that is not 0 or 1 or b is NULL
+ * binary_to_uint - convert binary number to an unsigned integer.
+ * @b: binary number
+ * Return: unsigned integer
  */
+ 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int number;
-	int i, len, pow_of_2;
+	unsigned int unsigned_integer;
+	int length, base2;
 
-	len = _strlen(b);
-	number = 0;
-	pow_of_2 = 1;
-	if (b == NULL)
-		return (0);
-	for (i = len - 1; i >= 0 ; i--)
+	if (!b)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		if (b[i] == '0')
-			number += 0;
-		if (b[i] == '1')
-			number += pow_of_2;
-		pow_of_2 *= 2;
+		return (0);
 	}
-	return (number);
+
+	unsigned_integer = 0;
+
+	for (length = 0; b[length] != '\0'; length++)
+		;
+
+	for (length--, base2 = 1; length >= 0; length--, base2 *= 2)
+	{
+		if (b[length] != '0' && b[length] != '1')
+		{
+			return (0);
+		}
+
+		if (b[length] & 1)
+		{
+			unsigned_integer += base2;
+		}
+	}
+
+	return (unsigned_integer);
 }
